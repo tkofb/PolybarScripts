7b9b9782ce893c3b6926cdfab69d6310ff7dd5d5
tkofb <benard.kihiuria@gmail.com>
2023-08-02 16:59:26 -0400

    Decided to go back to open weather for its weather descriptions for a changeable symbol

--- weather.py
+++ weather.py
@@ -1,1 +1,28 @@
-print("0")
+import requests
+
+
+params = { 
+    'access_key': '3735019f0d3687983087e3b0c04e9d38',
+    'query': 'Salisbury, Maryland'
+}
+
+
+def celsiusToFahrenheit(num):
+    convertedNum = (num*1.8) + 32
+    return convertedNum
+
+request = requests.get(url="http://api.weatherstack.com/current", params = params)
+
+
+try: 
+    if request.status_code == 200:
+        currentTemp = request.json()['current']["temperature"]
+        currentTemp = celsiusToFahrenheit(currentTemp)
+        returnString = f"{currentTemp} °F"
+        print(returnString)
+       
+except:
+    print("0")
+    
+
+
